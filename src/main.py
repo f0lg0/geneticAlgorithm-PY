29d0e63b01fc9cc55b243d434891f9aa2f3f7cf6
philippeitis <33013301+philippeitis@users.noreply.github.com>
2020-02-04 12:15:35 -0800

    Get rid of magic methods, simplify code
    This gets rid of __len__ and __str__ calls, and uses for loops where applicable.

--- src/main.py
+++ src/main.py
@@ -13,37 +13,30 @@ KEYS = 'abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 class Individual:
 	def __init__(self):
-		self._dna = []
+		self._dna = random.choices(KEYS, k=INDIVIDUAL_SIZE)
 		self._fitness = 0
-
-		i = 0
-		while i < INDIVIDUAL_SIZE:
-			self._dna.append(random.choice(KEYS))
-			i += 1
 
 	def getDNA(self):
 		return self._dna
 
 	def calculateFitness(self):
 		self._score = 0
-		for i in range(self._dna.__len__()):
-			if self._dna[i] == TARGET[i]:
+		for own_letter, target_letter in zip(self._dna, TARGET):
+			if own_letter == target_letter:
 				self._score += 1
 
-		self._fitness = self._score / TARGET.__len__()
+		self._fitness = self._score / len(TARGET)
 		return self._fitness
 
 	def __str__(self):
-		return self._dna.__str__()
+		return str(self._dna)
 
 class Population:
 	def __init__(self, size):
 		self._population = []
 
-		i = 0
-		while i < size:
+		for i in range(size):
 			self._population.append(Individual())
-			i += 1
 
 	def getPopulation(self):
 		return self._population
@@ -52,15 +45,13 @@ class GeneticAlgorithm:
 	def selectTournamentPopulation(self, pop):
 		tournament_pop = Population(0)
 
-		i = 0
-		while i < TOURNAMENT_SELECTION_SIZE:
+		for i in range(TOURNAMENT_SELECTION_SIZE):
 			tournament_pop.getPopulation().append(pop.getPopulation()[random.randrange(0, POPULATION_SIZE)])
-			i += 1
 			tournament_pop.getPopulation().sort(key = lambda x: x.calculateFitness(), reverse = True)
 		return tournament_pop
 
 	def reproduction(self, pop):
-		for i in range(pop.getPopulation().__len__()):
+		for i in range(len(pop.getPopulation()):
 			self._partnerA = self.selectTournamentPopulation(pop).getPopulation()[0]	
 			self._partnerB = self.selectTournamentPopulation(pop).getPopulation()[1]	
 
@@ -73,7 +64,7 @@ class GeneticAlgorithm:
 		self._child = Individual()
 		self._midpoint = random.randrange(0, INDIVIDUAL_SIZE)
 
-		for i in range(TARGET.__len__()):
+		for i in range(len(TARGET)):
 			if i < self._midpoint:
 				self._child.getDNA()[i] = parentA.getDNA()[i]
 			else:
@@ -96,10 +87,8 @@ def printPopulation(pop, genNumber):
 	print("Generation #", genNumber, "| Fittest individual fitness: ", pop.getPopulation()[0].calculateFitness())
 	print("Target phrase:", TARGET)
 	print("==========================================================")
-	i = 0
-	for x in pop.getPopulation():
+	for i, x in enumerate(pop.getPopulation()):
 		print("Individual #", i, ":", ''.join(x.getDNA()), "| Fitness: ", x.calculateFitness())
-		i += 1
 	print()
 
 def main():
